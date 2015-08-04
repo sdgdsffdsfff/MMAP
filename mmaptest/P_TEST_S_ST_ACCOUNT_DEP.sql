2c2c3438c3ecf92ee4f427d9761fc5c3bc589097
lee5hx <sales@originyin.com>
2015-08-04 15:26:28 +0800

    add test dep,loan

---
+++ mmaptest/P_TEST_S_ST_ACCOUNT_DEP.sql
@@ -0,0 +1,147 @@
+CREATE OR REPLACE PROCEDURE "P_TEST_S_ST_ACCOUNT_DEP"
+  AS
+  
+TRANS_NAME VARCHAR2(50) :='P_TEST_S_ST_ACCOUNT_DEP' ; --存储过程名
+
+V_TABLE_NAME VARCHAR2(50) :='ST_ACCOUNT_DEP';  -- 测试目标表名
+V_COL VARCHAR2(50);     --字段英文名
+V_COL_NAME VARCHAR2(50);     --字段中文名
+V_COL_DES VARCHAR2(50);     --字段描述
+V_TEST_DES VARCHAR2(50);     --测试描述
+V_TEST_STAGE VARCHAR2(50) :='S-ST';  -- 测试阶段
+V_TEST_TIMESTAMP TIMESTAMP;  -- 加载测试时间
+
+--存储过程日志变量
+V_START_TIMESTAMP TIMESTAMP;  -- 加载开始时间
+V_END_TIMESTAMP    TIMESTAMP;  -- 加载结束时间
+IO_ROW INTEGER;
+IO_STATUS INTEGER;
+VO_SQLERR VARCHAR2(5000);          -- 返回的描述
+
+BEGIN
+SELECT SYSDATE INTO V_START_TIMESTAMP FROM dual;  -- 加载程序运行开始时间
+V_TEST_TIMESTAMP := V_START_TIMESTAMP;
+
+--1.字符型字段
+--如果表中的字符型数据没有特殊处理，则仅测试表记录数即可;如果有特殊处理，则再做个别字段逻辑测试。
+--表记录数
+V_COL := 'TABLE_COUNT';
+V_COL_NAME := '记录数';
+V_COL_DES := '表中记录数';
+V_TEST_DES := '计数';
+
+INSERT INTO MMAPTEST.MMAP_TEST
+  (
+    TEST_DATE          --  测试时间
+    ,TEST_STAGE        --  测试阶段
+    ,TABLE_NAME      --  目标表名
+    ,TX_DATE      --  数据日期
+    ,COL      -- 字段英文名
+    ,COL_NAME    --  字段中文名
+    ,COL_DES    --  字段描述
+    ,TEST_DES    --  测试描述
+    ,VALUE_SOURCE    -- 字段值（源）
+    ,VALUE_TARGET    --  字段值（目标）
+    ,COMPARE_RESULT    --  对比结果
+    ,REMARK    --  备注
+
+  )
+select 
+V_TEST_TIMESTAMP
+,V_TEST_STAGE
+,V_TABLE_NAME
+,COALESCE(T1.tx_dt,T2.TX_DATE)
+,V_COL
+,V_COL_NAME
+,V_COL_DES
+,V_TEST_DES
+,T1.Source_NUM
+,T2.ST_NUM
+,NVL(T1.Source_NUM,0)-nvl(T2.ST_NUM,0)
+,NULL
+FROM 
+(
+select A.Tx_DT,COUNT(1) AS Source_NUM    --源修改
+from   MMAPST.DMMKT_ACLIABI A
+GROUP BY Tx_DT
+) T1
+FULL JOIN 
+(
+SELECT A.TX_DATE,COUNT(1) AS ST_NUM  --目标修改
+FROM   MMAPST.ST_ACCOUNT_DEP A
+GROUP BY TX_DATE
+)T2
+ON T1.Tx_DT=T2.TX_DATE;
+
+--2.数值型字段
+--对经过特殊处理的每个数值型字段做测试；如果表中该类字段均未做特殊处理，则随机取出一个数值型字段做测试；如果表中没有该类字段，则跳过。
+--ACCT_Bal
+V_COL := 'ACCT_Bal';
+V_COL_NAME := '账户余额';
+V_COL_DES := '';
+V_TEST_DES := '求和';
+
+INSERT INTO MMAPTEST.MMAP_TEST
+  (
+    TEST_DATE          --  测试时间
+    ,TEST_STAGE        --  测试阶段
+    ,TABLE_NAME      --  目标表名
+    ,TX_DATE      --  数据日期
+    ,COL      -- 字段英文名
+    ,COL_NAME    --  字段中文名
+    ,COL_DES    --  字段描述
+    ,TEST_DES    --  测试描述
+    ,VALUE_SOURCE    -- 字段值（源）
+    ,VALUE_TARGET    --  字段值（目标）
+    ,COMPARE_RESULT    --  对比结果
+    ,REMARK    --  备注
+
+  )
+select 
+V_TEST_TIMESTAMP
+,V_TEST_STAGE
+,V_TABLE_NAME
+,COALESCE(T1.tx_dt,T2.TX_DATE)
+,V_COL
+,V_COL_NAME
+,V_COL_DES
+,V_TEST_DES
+,T1.DM_NUM
+,T2.ST_NUM
+,NVL(T1.DM_NUM,0)-nvl(T2.ST_NUM,0)
+,NULL
+FROM 
+(
+select A.Tx_DT,SUM(A.Acc_Bal) AS DM_NUM --修改
+from   MMAPST.DMMKT_ACLIABI A
+GROUP BY Tx_DT
+) T1
+FULL JOIN 
+(
+SELECT A.TX_DATE,SUM(A.ACCT_Bal) AS ST_NUM  --修改
+FROM   MMAPST.ST_ACCOUNT_DEP A
+GROUP BY TX_DATE
+)T2
+ON T1.Tx_DT=T2.TX_DATE;
+
+
+IO_ROW := SQL%ROWCOUNT ; --记录存储过程记录数
+COMMIT;
+SELECT SYSDATE INTO  V_END_TIMESTAMP  FROM dual;  -- 加载程序运行结束时间
+IO_STATUS := 0 ;
+VO_SQLERR := 'SUSSCESS';
+
+P_TEST_WRITE_LOGS(TRANS_NAME,IO_STATUS,IO_ROW,V_START_TIMESTAMP,V_END_TIMESTAMP,VO_SQLERR);
+
+EXCEPTION
+    WHEN OTHERS THEN
+  ROLLBACK ;
+  IO_STATUS := 9 ;
+  VO_SQLERR := SQLCODE ||  SQLERRM  ;
+  SELECT SYSDATE INTO  V_END_TIMESTAMP  FROM dual;
+  P_TEST_WRITE_LOGS(TRANS_NAME,IO_STATUS,IO_ROW,V_START_TIMESTAMP,V_END_TIMESTAMP,VO_SQLERR);
+  
+
+
+END  P_TEST_S_ST_ACCOUNT_DEP;
+/
