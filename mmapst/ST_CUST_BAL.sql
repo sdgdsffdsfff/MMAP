9cefbd578161657af70adf63a3d2eceff170987e
panpanguo <guopanpan@originyin.com>
2015-08-11 11:10:04 +0800

    UPDATE COLUMN NAME
    UPDATE COLUMN NAME

--- mmapst/ST_CUST_BAL.sql
+++ mmapst/ST_CUST_BAL.sql
@@ -10,21 +10,21 @@ INSERT INTO ST_CUST_BAL
   ,TX_DATE                    --数据日期
   ,period_ID                  --日期
   ,CUSTOMER_ID                --客户号
-  ,CUST_LC_CD_BAL             --活期存款余额（人民币）
-  ,CUST_LC_TD_BAL             --定期存款余额（人民币）
-  ,CUST_LC_LOAN_BAL           --贷款余额（人民币）
-  ,CUST_LC_NDEBT_BAL          --国债余额（人民币）
-  ,CUST_LC_FOUND_BAL          --基金余额（人民币）
-  ,CUST_LC_FIN_BAL            --理财余额（人民币）
-  ,CUST_LC_INSURE_BAL         --保险余额（人民币）
-  ,CUST_LC_NOBLE_BAL          --贵金属余额（人民币）
-  ,CUST_LC_CREDIT_BAL         --信用卡消费额（人民币）
-  ,CUST_LC_ALL_BAL            --客户总资产余额（人民币）
-  ,CUST_FC_CD_BAL             --活期存款余额（外币折合人民币）
-  ,CUST_FC_TD_BAL             --定期存款余额（外币折合人民币）
-  ,CUST_FC_LOAN_BAL           --贷款余额（外币折合人民币）
-  ,CUST_FC_CREDIT_BAL         --信用卡消费额（外币折合人民币）
-  ,CUST_FC_ALL_BAL            --客户总资产余额（外币折合人民币）
+  ,CUST_CD_BAL_LC             --活期存款余额（人民币）
+  ,CUST_TD_BAL_LC             --定期存款余额（人民币）
+  ,CUST_LOAN_BAL_LC           --贷款余额（人民币）
+  ,CUST_NDEBT_BAL_LC          --国债余额（人民币）
+  ,CUST_FOUND_BAL_LC          --基金余额（人民币）
+  ,CUST_FIN_BAL_LC            --理财余额（人民币）
+  ,CUST_INSURE_BAL_LC         --保险余额（人民币）
+  ,CUST_NOBLE_BAL_LC          --贵金属余额（人民币）
+  ,CUST_CREDIT_BAL_LC         --信用卡消费额（人民币）
+  ,CUST_ALL_BAL_LC            --客户总资产余额（人民币）
+  ,CUST_CD_BAL_FC             --活期存款余额（外币折合人民币）
+  ,CUST_TD_BAL_FC             --定期存款余额（外币折合人民币）
+  ,CUST_LOAN_BAL_FC           --贷款余额（外币折合人民币）
+  ,CUST_CREDIT_BAL_FC         --信用卡消费额（外币折合人民币）
+  ,CUST_ALL_BAL_FC            --客户总资产余额（外币折合人民币）
   ,CUST_ALL_BAL               --客户总资产余额（人民币+外币）
 )
 
